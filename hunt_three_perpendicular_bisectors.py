e1263bbabefd8ad877b0e445b9a9c1df6931dadd
Nikolay Pultsin <geometer@fbreader.org>
2020-02-18 09:13:41 +0000

    perpendicular bisector

---
+++ hunt_three_perpendicular_bisectors.py
@@ -0,0 +1,20 @@
+#!/var/www/sandbox/virtualenv/bin/python
+
+from sandbox import *
+from sandbox.hunter import hunt
+
+scene = Scene()
+
+A = scene.free_point(label='A')
+B = scene.free_point(label='B')
+C = scene.free_point(label='C')
+perp_AB = scene.perpendicular_bisector_line(A, B)
+perp_AC = scene.perpendicular_bisector_line(A, C)
+perp_BC = scene.perpendicular_bisector_line(B, C)
+O = perp_AB.intersection_point(perp_AC, label='O')
+P = perp_AB.intersection_point(perp_BC, label='P')
+Q = perp_AC.intersection_point(perp_BC, label='Q')
+
+print(scene)
+
+hunt(scene, 'coincidences')
--- sandbox/scene.py
+++ sandbox/scene.py
@@ -52,3 +52,16 @@ class Scene(CoreScene):
         tmp2 = line.intersection_point(circle, auxiliary=True)
         tmp2.add_constraint(Constraint.Kind.not_equal, tmp)
         return tmp.ratio_point(tmp2, 1, 1, **kwargs)
+
+    def perpendicular_bisector_line(self, point0, point1, **kwargs):
+        """
+        The perpendicular bisector to the segment [point0, point1]
+        """
+        self.assert_point(point0)
+        self.assert_point(point1)
+        circle0 = point0.circle_through(point1, auxiliary=True)
+        circle1 = point1.circle_through(point0, auxiliary=True)
+        tmp0 = circle0.intersection_point(circle1, auxiliary=True)
+        tmp1 = circle0.intersection_point(circle1, auxiliary=True)
+        tmp1.add_constraint(Constraint.Kind.not_equal, tmp0)
+        return tmp0.line_through(tmp1, **kwargs)
