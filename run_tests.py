be453fa8eca1f811b5c4a8378f2283c4011e3c7b
Nikolay Pultsin <geometer@fbreader.org>
2020-02-21 08:40:34 +0000

    more tests from samples

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ import unittest
 from tests.napoleon import *
 from tests.triangle345 import *
 from tests.rog4578 import *
+from tests.mccme7 import *
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/mccme7.py
@@ -0,0 +1,49 @@
+# Problem 7 from http://zadachi.mccme.ru/2012/
+
+import math
+
+from sandbox import Scene, iterative_placement
+
+from .base import SandboxTest
+
+class TestTriangleMCCME7_long(SandboxTest):
+    def setUp(self):
+        scene = Scene()
+
+        O = scene.free_point(label='O')
+        circle = O.circle_through(scene.free_point(auxiliary=True))
+        A = circle.free_point(label='A')
+        B = circle.free_point(label='B')
+        C = circle.free_point(label='C')
+        D = circle.free_point(label='D')
+        scene.quadrilateral_constraint(A, B, C, D)
+        A.distance_constraint(B, 3)
+        B.distance_constraint(C, 4)
+        C.distance_constraint(D, 5)
+        A.distance_constraint(D, 2)
+
+        self.placement = iterative_placement(scene)
+
+    def test1(self):
+        self.assertDistance('A', 'C', math.sqrt(299.0 / 11))
+
+class TestTriangleMCCME7_quick(SandboxTest):
+    def setUp(self):
+        scene = Scene()
+
+        O = scene.free_point(label='O', x=0, y=0)
+        A = scene.free_point(label='A')
+        circle = O.circle_through(A)
+        B = circle.free_point(label='B')
+        C = circle.free_point(label='C')
+        D = circle.free_point(label='D')
+        scene.quadrilateral_constraint(A, B, C, D)
+        A.distance_constraint(B, 3)
+        B.distance_constraint(C, 4)
+        C.distance_constraint(D, 5)
+        A.distance_constraint(D, 2)
+
+        self.placement = iterative_placement(scene)
+
+    def test1(self):
+        self.assertDistance('A', 'C', math.sqrt(299.0 / 11))
