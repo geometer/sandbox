906fa22f54edbe0800a34a138b3af128bc335d87
Nikolay Pultsin <geometer@fbreader.org>
2020-02-24 08:24:40 +0000

    PlacementTest.ERROR

--- tests/base.py
+++ tests/base.py
@@ -3,6 +3,8 @@ import unittest
 import mpmath
 
 class PlacementTest(unittest.TestCase):
+    ERROR = 1e-6
+
     def setUp(self):
         self.startTime = time.time()
         self.placement = self.createPlacement()
@@ -14,48 +16,48 @@ class PlacementTest(unittest.TestCase):
         loc = self.placement.location(pt)
         self.assertLess(
             mpmath.fabs(loc.x - x) + mpmath.fabs(loc.y - y),
-            1e-6,
+            PlacementTest.ERROR,
             '%s != (%s,%s)' % (loc, x, y)
         )
 
     def assertDistance(self, pt0, pt1, distance):
         self.assertLess(
             mpmath.fabs(self.placement.distance(pt0, pt1) - distance),
-            1e-6,
+            PlacementTest.ERROR,
             '|%s,%s| != %d' % (pt0, pt1, distance)
         )
 
     def assertEqualDistances(self, pt0, pt1, pt2, pt3):
         self.assertLess(
             mpmath.fabs(self.placement.distance(pt0, pt1) - self.placement.distance(pt2, pt3)),
-            1e-6,
+            PlacementTest.ERROR,
             '|%s,%s| != |%s,%s|' % (pt0, pt1, pt2, pt3)
         )
 
     def assertDistanceRatio(self, pt0, pt1, pt2, pt3, ratio):
         self.assertLess(
             mpmath.fabs(self.placement.distance(pt0, pt1) - self.placement.distance(pt2, pt3) * ratio),
-            1e-6,
+            PlacementTest.ERROR,
             '|%s,%s| != |%s,%s| * %s' % (pt0, pt1, pt2, pt3, ratio)
         )
 
     def assertAngle(self, pt0, pt1, pt2, pt3, value):
         self.assertLess(
             mpmath.fabs(mpmath.fabs(self.placement.angle(pt0, pt1, pt2, pt3)) - value),
-            1e-6,
+            PlacementTest.ERROR,
             '∠(%s,%s),(%s,%s) != %s' % (pt0, pt1, pt2, pt3, value)
         )
 
     def assertEqualSignedAngles(self, pt0, pt1, pt2, pt3, pt4, pt5, pt6, pt7):
         self.assertLess(
             mpmath.fabs(self.placement.angle(pt0, pt1, pt2, pt3) - self.placement.angle(pt4, pt5, pt6, pt7)),
-            1e-6,
+            PlacementTest.ERROR,
             '∠(%s,%s),(%s,%s) != ∠(%s,%s),(%s,%s)' % (pt0, pt1, pt2, pt3, pt4, pt5, pt6, pt7)
         )
 
     def assertRadius(self, circle, radius):
         self.assertLess(
             mpmath.fabs(self.placement.radius(circle) - radius),
-            1e-6,
+            PlacementTest.ERROR,
             'radius(%s) != %s' % (circle, radius)
         )
