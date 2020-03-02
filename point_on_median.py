db230c0e219c3244a02fe28660d2c505f4850483
Nikolay Pultsin <geometer@fbreader.org>
2020-03-02 08:52:44 +0000

    updated sample

--- point_on_median.py
+++ point_on_median.py
@@ -1,12 +1,13 @@
 from sandbox import *
 from sandbox.hunter import Hunter
+from sandbox.explainer import Explainer
 
 scene = Scene()
 
 A = scene.free_point(label='A')
 B = scene.free_point(label='B')
 C = scene.free_point(label='C')
-M = scene.gravity_centre_point(A, B, label='M')
+M = scene.middle_point(A, B, label='M')
 l = C.line_through(M)
 D = l.free_point(label='D')
 para = scene.parallel_line(A.line_through(B), D)
@@ -14,6 +15,9 @@ A1 = para.intersection_point(A.line_through(C), label='A1')
 B1 = para.intersection_point(B.line_through(C), label='B1')
 
 hunter = Hunter(scene)
-hunter.hunt('all')
+hunter.hunt()
 print('')
-hunter.dump()
+
+explainer = Explainer(scene, hunter.properties)
+explainer.explain()
+explainer.dump()
