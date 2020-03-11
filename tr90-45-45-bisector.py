b9df55518ce3865dbdd29713c4aea63539c9a7bf
Nikolay Pultsin <geometer@fbreader.org>
2020-03-11 22:38:09 +0000

    missing samples

---
+++ tr90-45-45-bisector.py
@@ -0,0 +1,24 @@
+# https://www.facebook.com/groups/parmenides52/, problem 4594
+
+from sandbox import Scene, iterative_placement
+from sandbox.hunter import Hunter
+from sandbox.explainer import Explainer
+
+scene = Scene()
+
+A, B, C = scene.triangle(labels=('A', 'B', 'C'))
+scene.perpendicular_constraint((A, C), (B, C), comment='Given: AC ⟂ BC')
+A.vector(C).length_ratio_constraint(B.vector(C), 1, comment='Given: AC = BC')
+bisector = C.angle(A, B).bisector_line()
+D = bisector.intersection_point(A.line_through(B), label='D')
+
+placement = iterative_placement(scene)
+
+hunter = Hunter(placement)
+hunter.hunt()
+print('')
+
+explainer = Explainer(scene, hunter.properties)
+explainer.explain()
+explainer.dump()
+explainer.stats().dump()
