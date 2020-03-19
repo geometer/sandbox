97c116259111fe255ad15136dc402e2e163793c1
Nikolay Pultsin <geometer@fbreader.org>
2020-03-19 00:14:00 +0000

    more samples

---
+++ equilateral-orthocentre.py
@@ -0,0 +1,20 @@
+#!/var/www/sandbox/virtualenv/bin/python
+
+from sandbox import Scene
+from sandbox.hunter import Hunter
+from sandbox.explainer import Explainer
+
+scene = Scene()
+
+A, B, C = scene.triangle(labels=['A', 'B', 'C'])
+A.vector(C).length_equal_constraint(A.vector(B))
+B.vector(C).length_equal_constraint(A.vector(B))
+scene.orthocentre_point((A, B, C), label='D')
+
+hunter = Hunter(scene)
+hunter.hunt()
+
+explainer = Explainer(scene, hunter.properties)
+explainer.explain()
+explainer.dump()
+explainer.stats().dump()
