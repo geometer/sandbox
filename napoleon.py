37565e722aaec8185a5e8cf645f37037db5f8b20
Nikolay Pultsin <geometer@fbreader.org>
2020-04-05 06:33:33 +0100

    no hunter in napoleon sample

--- napoleon.py
+++ napoleon.py
@@ -1,7 +1,6 @@
 import sys
 
 from sandbox import Scene
-from sandbox.hunter import Hunter
 from sandbox.explainer import Explainer
 from sandbox.propertyset import PropertySet
 from sandbox.util import _comment
@@ -10,8 +9,9 @@ scene = Scene()
 
 A, B, C = scene.triangle(labels=['A', 'B', 'C'])
 
-def napoleonic(A: Scene.Point, B: Scene.Point, C: Scene.Point):
-    V = A.circle_through(B).intersection_point(B.circle_through(A), label=C.label + '1')
+def napoleonic(A, B, C):
+    V = A.scene.free_point(label=C.label + '1')
+    A.scene.equilateral_constraint((A, B, V), comment='Given')
     line = A.line_through(B, layer='auxiliary')
     V.opposite_side_constraint(C, line, comment=_comment('Given: %s is outward of △ %s %s %s', V, A, B, C))
     D = scene.incentre_point((A, B, V), label=C.label + '2')
@@ -20,14 +20,9 @@ napoleonic(A, B, C)
 napoleonic(C, A, B)
 napoleonic(B, C, A)
 
-hunter = Hunter(scene)
-hunter.hunt()
-
 angle = scene.get('A2').angle(scene.get('B2'), scene.get('C2'))
 
-print('\tGuessed: %s = %s' % (angle, hunter.guessed(angle)))
-
-explainer = Explainer(scene, hunter.properties)
+explainer = Explainer(scene, [])
 if '--profile' in sys.argv[1:]:
     import cProfile
     cProfile.run('explainer.explain()')
