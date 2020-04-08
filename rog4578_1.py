73cc213586b58bb815e115152bd81ff4d5c797a0
Nikolay Pultsin <geometer@fbreader.org>
2020-04-08 07:39:52 +0100

    sample rewritten to use standard technique

--- rog4578_1.py
+++ rog4578_1.py
@@ -1,21 +1,17 @@
 # "Romantics of Geometry" group on Facebook, problem 4578
 # https://www.facebook.com/groups/parmenides52/permalink/2779763428804012/
 
-import math
-import sys
-
+from runner import run_sample
 from sandbox import *
-from sandbox.hunter import Hunter
-from sandbox.explainer import Explainer
-from sandbox.propertyset import PropertySet
+from sandbox.property import AngleValueProperty
 
 scene = Scene()
 
 A, B, C = scene.triangle(labels=('A', 'B', 'C'))
 D = scene.orthocentre_point((A, B, C), label='D')
 D.inside_triangle_constraint(A, B, C)
-H = A.line_through(D).intersection_point(B.line_through(C), label='H')
-G = C.line_through(D).intersection_point(A.line_through(B), label='G')
+#H = A.line_through(D).intersection_point(B.line_through(C), label='H')
+#G = C.line_through(D).intersection_point(A.line_through(B), label='G')
 A.segment(B).congruent_constraint(C.segment(D), comment='Given: |AB| = |CD|')
 
 #Proof
@@ -37,50 +33,6 @@ A.segment(B).congruent_constraint(C.segment(D), comment='Given: |AB| = |CD|')
 # +*16* ∠ A C B = ∠ A C H                          *same angle
 # +*17* ∠ A C B = ∠ A C H = 45º                    *15, *16           transitivity
 
-angle = C.angle(A, B)
+prop = AngleValueProperty(C.angle(A, B), 45)
 
-placement = iterative_placement(scene)
-print('\tMeasured: %s = %.5f' % (angle, placement.angle(angle) / math.pi * 180))
-
-hunter = Hunter(placement)
-hunter.hunt()
-hunter.stats().dump()
-print('\tGuessed: %s = %s' % (angle, hunter.guessed(angle)))
-
-explainer = Explainer(scene)
-if '--profile' in sys.argv[1:]:
-    import cProfile
-    cProfile.run('explainer.explain()')
-else:
-    explainer.explain()
-explainer.stats(hunter.properties).dump()
-print('\tExplained: %s = %s' % (angle, explainer.explained(angle)))
-
-if '--explain' in sys.argv[1:]:
-    def dump(prop, level=0):
-        print('\t' + '  ' * level + str(prop) + ': ' + ' + '.join([str(com) for com in prop.reason.comments]))
-        if prop.reason.premises:
-            for premise in prop.reason.premises:
-                dump(premise, level + 1)
-
-    def depth(prop):
-        if prop.reason.premises:
-            return 1 + max(depth(p) for p in prop.reason.premises)
-        return 0
-
-    def all_premises(prop):
-        premises = PropertySet()
-        def collect(p):
-            premises.add(p)
-            if p.reason.premises:
-                for pre in p.reason.premises:
-                    collect(pre)
-        collect(prop)
-        return premises
-
-    explanation = explainer.explanation(angle)
-    if explanation:
-        dump(explanation)
-        print('Depth = %s' % depth(explanation))
-        print('Props = %s' % len(all_premises(explanation)))
-        all_premises(explanation).stats().dump()
+run_sample(scene, prop)
