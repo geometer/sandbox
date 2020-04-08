e298ebf059c844099560892a9582b7b5a0d8d016
Nikolay Pultsin <geometer@fbreader.org>
2020-04-08 15:19:33 +0100

    do not use complex explanation if simple is available

--- python/sandbox/rules/basic.py
+++ python/sandbox/rules/basic.py
@@ -195,17 +195,19 @@ class SumAndRatioOfTwoAnglesRule(SingleSourceRule):
     property_type = SumOfAnglesProperty
 
     def apply(self, prop, context):
+        if context.angle_value_property(prop.angle0) or context.angle_value_property(prop.angle1):
+            return
         ar = context.angles_ratio_property(prop.angle0, prop.angle1)
         if ar is None or prop.reason.obsolete and ar.reason.obsolete:
             return
         value1 = divide(prop.degree, 1 + ar.value)
         value0 = prop.degree - value1
         if ar.value == 1:
-            comment0 = _comment('%s + %s = %s', ar.angle0, ar.angle0, prop.degree),
-            comment1 = _comment('%s + %s = %s', ar.angle1, ar.angle1, prop.degree),
+            comment0 = _comment('%s + %s = %s + %s = %sº', ar.angle0, ar.angle0, ar.angle0, ar.angle1, prop.degree),
+            comment1 = _comment('%s + %s = %s + %s = %sº', ar.angle1, ar.angle1, ar.angle1, ar.angle0, prop.degree),
         else:
-            comment0 = _comment('%s + %s / %s = %s', ar.angle0, ar.angle0, ar.value, prop.degree),
-            comment1 = _comment('%s + %s %s = %s', ar.angle1, ar.value, ar.angle1, prop.degree),
+            comment0 = _comment('%s + %s / %s = %s + %s = %sº', ar.angle0, ar.angle0, ar.value, ar.angle0, ar.angle1, prop.degree),
+            comment1 = _comment('%s + %s %s = %s + %s = %sº', ar.angle1, ar.value, ar.angle1, ar.angle1, ar.angle0, prop.degree),
         yield (AngleValueProperty(ar.angle0, value0), comment0, [prop, ar])
         yield (AngleValueProperty(ar.angle1, value1), comment1, [prop, ar])
 
