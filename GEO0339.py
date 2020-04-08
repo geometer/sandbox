53cc347ecc4e7fbc8a03bebe1a52fdaa4796e46a
Nikolay Pultsin <geometer@fbreader.org>
2020-04-08 11:22:12 +0100

    sample uses standard runner

--- GEO0339.py
+++ GEO0339.py
@@ -1,9 +1,8 @@
 # GEO0339 from http://hilbert.mat.uc.pt/TGTP/Problems/listing.php
-import sys
 
+from runner import run_sample
 from sandbox import *
-from sandbox.hunter import Hunter
-from sandbox.explainer import Explainer
+from sandbox.property import LengthRatioProperty
 
 scene = Scene()
 
@@ -13,17 +12,6 @@ A1 = scene.circumcentre_point((H, B, C), label='A1')
 B1 = scene.circumcentre_point((H, A, C), label='B1')
 C1 = scene.circumcentre_point((H, A, B), label='C1')
 
-placement = iterative_placement(scene)
+prop = LengthRatioProperty(A.segment(B), A1.segment(B1), 1)
 
-hunter = Hunter(placement)
-hunter.hunt()
-print('')
-
-explainer = Explainer(scene)
-explainer.explain()
-if '--dump' in sys.argv[1:]:
-    explainer.dump(hunter.properties)
-explainer.stats(hunter.properties).dump()
-
-helper = PlacementHelper(placement)
-print('%.5f = %.5f' % (helper.distance(A, B), helper.distance(A1, B1)))
+run_sample(scene, prop)
