da7c374273824f82d78c65da08fcdab97e2fa796
Nikolay Pultsin <geometer@fbreader.org>
2020-04-12 16:41:23 +0100

    uses new AngleKindProperty

--- python/altitudes.py
+++ python/altitudes.py
@@ -1,7 +1,7 @@
 from sandbox import Scene
 from sandbox.explainer import Explainer
 from sandbox.property import SimilarTrianglesProperty
-from sandbox.property import AcuteAngleProperty, ObtuseAngleProperty, AngleValueProperty, PointsCollinearityProperty
+from sandbox.property import AngleKindProperty, PointsCollinearityProperty
 from sandbox.propertyset import ContradictionError
 from sandbox.reason import Reason
 
@@ -13,7 +13,7 @@ altitudeB = scene.altitude((A, B, C), B)
 A1 = altitudeA.intersection_point(B.line_through(C), label='A1')
 B1 = altitudeB.intersection_point(A.line_through(C), label='B1')
 
-B.angle(A, C).is_obtuse_constraint(comment='Test')
+#B.angle(A, C).is_obtuse_constraint(comment='Test')
 #C.angle(A, B).is_obtuse_constraint(comment='Test')
 #C.angle(A, B).is_right_constraint(comment='Test')
 
@@ -25,15 +25,7 @@ def angles(context):
         collection.add(ncl.points[0].angle(ncl.points[1], ncl.points[2]))
         collection.add(ncl.points[1].angle(ncl.points[0], ncl.points[2]))
         collection.add(ncl.points[2].angle(ncl.points[0], ncl.points[1]))
-    def in_use(angle):
-        if AcuteAngleProperty(angle) in context:
-            return True
-        if ObtuseAngleProperty(angle) in context:
-            return True
-        if AngleValueProperty(angle, 90) in context:
-            return True
-        return False
-    return [angle for angle in collection if not in_use(angle)]
+    return [angle for angle in collection if context.angle_kind_property(angle) is None]
 
 explainer0 = Explainer(scene)
 explainer0.explain()
@@ -44,9 +36,11 @@ else:
         print('Trying variants for: %s' % angle)
         success_count = 0
         contradiction_count = 0
-        for assumption in (AngleValueProperty(angle, 90), AcuteAngleProperty(angle), ObtuseAngleProperty(angle)):
+        for kind in AngleKindProperty.Kind:
             try:
                 explainer = Explainer(scene, base=explainer0)
+                assumption = AngleKindProperty(angle, kind)
+                print('Assume: %s' % assumption)
                 assumption.reason = Reason(-2, -2, 'Assumption', [])
                 assumption.reason.obsolete = False
                 explainer.context.add(assumption)
