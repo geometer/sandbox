c79402d4b7604e2eb182f4689150302b3c4de809
Nikolay Pultsin <geometer@fbreader.org>
2020-04-18 18:06:24 +0100

    congruent triangles by angle and two sides

--- python/sandbox/rules/triangles.py
+++ python/sandbox/rules/triangles.py
@@ -60,6 +60,48 @@ class SimilarTrianglesByTwoAnglesRule(Rule):
                 [ca0, ca1, ncl]
             )
 
+class CongruentTrianglesByAngleAndTwoSidesRule(Rule):
+    def sources(self):
+        return self.context.congruent_angles_with_vertex()
+
+    def apply(self, src):
+        ang0, ang1 = src
+        if ang0.point_set == ang1.point_set:
+            return
+        ca = None
+
+        def congruent_segments(seg0, seg1):
+            if seg0 == seg1:
+                return True
+            return self.context.congruent_segments_property(seg0, seg1, True)
+
+        for vec0, vec1 in [(ang0.vector0, ang0.vector1), (ang0.vector1, ang0.vector0)]:
+            rsn0 = congruent_segments(vec0.as_segment, ang1.vector0.as_segment)
+            if rsn0 is None:
+                continue
+            rsn1 = congruent_segments(vec1.as_segment, ang1.vector1.as_segment)
+            if rsn1 is None:
+                continue
+            if ca is None:
+                ca = self.context.angle_ratio_property(ang0, ang1)
+            if ca.reason.obsolete and (rsn0 == True or rsn0.reason.obsolete) and (rsn1 == True or rsn1.reason.obsolete):
+                continue
+            if rsn0 == True:
+                comment = LazyComment('Common side %s, pair of congruent sides, and angle between the sides', vec0)
+                premises = [rsn1, ca]
+            elif rsn1 == True:
+                comment = LazyComment('Common side %s, pair of congruent sides, and angle between the sides', vec1)
+                premises = [rsn0, ca]
+            else:
+                comment = 'Two pairs of congruent sides, and angle between the sides'
+                premises = [rsn0, rsn1, ca]
+            yield (
+                CongruentTrianglesProperty(
+                    (ang0.vertex, vec0.points[1], vec1.points[1]),
+                    (ang1.vertex, ang1.vector0.end, ang1.vector1.end)
+                ), comment, premises
+            )
+
 class SimilarTrianglesByAngleAndTwoSidesRule(Rule):
     def sources(self):
         return [(a0, a1) for a0, a1 in self.context.congruent_angles_with_vertex() if a0.point_set != a1.point_set]
