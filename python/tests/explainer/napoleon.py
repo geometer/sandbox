4a0772398aebb69631d308361085f14e87b6d006
Nikolay Pultsin <geometer@fbreader.org>
2020-04-18 17:15:47 +0100

    use explicit equilateral construction in napooleon tests

--- python/tests/explainer/napoleon.py
+++ python/tests/explainer/napoleon.py
@@ -11,7 +11,9 @@ class NapoleonOutward(ExplainerTest):
         A, B, C = scene.triangle(labels=['A', 'B', 'C'])
 
         def napoleonic(A, B, C):
-            V = A.scene.free_point(label=C.label + '1')
+            circleAB = A.circle_through(B, layer='invisible')
+            circleBA = B.circle_through(A, layer='invisible')
+            V = circleAB.intersection_point(circleBA, label=C.label + '1')
             A.scene.equilateral_constraint((A, B, V), comment=LazyComment('Given: △ %s %s %s is an equilateral triangle', V, A, B))
             line = A.line_through(B, layer='auxiliary')
             V.opposite_side_constraint(C, line, comment=LazyComment('Given: %s is outward of △ %s %s %s', V, A, B, C))
@@ -34,7 +36,9 @@ class NapoleonInward(ExplainerTest):
         A, B, C = scene.triangle(labels=['A', 'B', 'C'])
 
         def napoleonic(A, B, C):
-            V = A.scene.free_point(label=C.label + '1')
+            circleAB = A.circle_through(B, layer='invisible')
+            circleBA = B.circle_through(A, layer='invisible')
+            V = circleAB.intersection_point(circleBA, label=C.label + '1')
             A.scene.equilateral_constraint((A, B, V), comment=LazyComment('Given: △ %s %s %s is an equilateral triangle', V, A, B))
             line = A.line_through(B, layer='auxiliary')
             V.same_side_constraint(C, line, comment=LazyComment('Given: %s is inward of △ %s %s %s', V, A, B, C))
