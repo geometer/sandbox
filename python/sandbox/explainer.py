6861979582ab7ad8d8333dc7470a3abffd150fc0
Nikolay Pultsin <geometer@fbreader.org>
2020-04-29 13:17:41 +0100

    fixed class name

--- python/sandbox/explainer.py
+++ python/sandbox/explainer.py
@@ -26,7 +26,7 @@ class Explainer:
         self.__rules = [
             LengthRatioTransitivityRule(self.context),
             ProportionalLengthsToLengthsRatioRule(self.context),
-            SumAndTwoAnglesInTriangle(self.context),
+            SumOfTwoAnglesInTriangle(self.context),
             SumAndRatioOfTwoAnglesRule(self.context),
             NonCollinearPointsAreDifferentRule(self.context),
             CoincidenceTransitivityRule(self.context),
