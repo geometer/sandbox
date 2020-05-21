8f1e59bd8dee30d725bc7f71e033824e280cafa8
Nikolay Pultsin <geometer@fbreader.org>
2020-05-21 13:42:31 +0100

    typo

--- python/visualise_akopyan_book_5_3_1.py
+++ python/visualise_akopyan_book_5_3_1.py
@@ -26,7 +26,7 @@ visualise(scene, prop, title='Problem 5.3.1', description=LazyComment('''
 <p>%s is a point inside segment %s.</p>
 <p>%s is a square.</p>
 <p>%s lies inside segment %s.</p>
-<p>%s in intersection of %s and %s.</p>
+<p>%s is the intersection point of %s and %s.</p>
 <p>Prove, that %s, %s, and %s are collinear.</p>
 <p style="font-size:80%%"><a href="http://vivacognita.org/555geometry.html/_/5/5-3/53-1-r371">Problem 5.3.1</a> from the <a href="https://www.amazon.com/Geometry-Figures-Second-Arseniy-Akopyan/dp/1548710784">Akopyan\'s book</a></p>
 ''', Scene.Polygon(A, B, C, D), E, B.segment(C), Scene.Polygon(B, E, F, K), B, A.segment(K), N, A.segment(C).as_line, E.segment(K).as_line, D, N, F).html())
