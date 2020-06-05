9c591ed808dc828bb80ff44e2753d01fa7e12291
Nikolay Pultsin <geometer@fbreader.org>
2020-06-06 00:01:20 +0100

    circles support in web view

--- python/sandbox/figure.py
+++ python/sandbox/figure.py
@@ -1,2 +1,21 @@
+from .util import LazyComment
+
 class Figure:
     pass
+
+class Circle(Figure):
+    def __init__(self, pt0, pt1, pt2):
+        self.points = (pt0, pt1, pt2)
+        self.__key = frozenset(self.points)
+
+    def css_class(self):
+        return LazyComment('circ__%s__%s__%s', *self.points)
+
+    def __str__(self):
+        return '○ %s %s %s' % self.points
+
+    def __eq__(self, other):
+        return self.__key == other.__key
+
+    def __hash__(self):
+        return hash(self.__key)
