a5991c6493d980708229e1af4c565fcba6ec9a6a
Nikolay Pultsin <geometer@fbreader.org>
2020-06-14 09:05:15 +0100

    centre_point for equilateral triangles; usage in napoleon samples

--- python/napoleon.py
+++ python/napoleon.py
@@ -9,25 +9,12 @@ triangle = scene.nondegenerate_triangle(labels=['A', 'B', 'C'])
 A, B, C = triangle.points
 
 def napoleonic(A, B, C):
-    # Change to True, if you want to construct the point explicitly.
-    # This change does not affect the explanation but makes placement a little bit quicker.
-    # Not a matter for the sample, but for more complicated construction this might be useful
-    # if you want to run the hunter.
-    constructive = False
-
-    label = '%s_1' % C.label
-    comment = LazyComment('Third vertex of equilateral triangle with base %s', A.segment(B))
-    if constructive:
-        circleAB = A.circle_through(B, layer='invisible')
-        circleBA = B.circle_through(A, layer='invisible')
-        V = circleAB.intersection_point(circleBA, label=label, comment=comment)
-    else:
-        V = A.scene.free_point(label=label, comment=comment)
-    equialteral = Scene.Triangle(A, B, V)
-    A.scene.equilateral_constraint(equialteral, comment=LazyComment('given: %s is equilateral', equialteral))
+    equilateral = scene.equilateral_triangle(A, B, '%s_1' % C.label)
+    _, _, C1 = equilateral.points
+    C1.comment = LazyComment('Third vertex of equilateral triangle with base %s', A.segment(B))
     line = A.line_through(B, layer='auxiliary')
-    V.opposite_side_constraint(C, line, comment=LazyComment('given: %s is outward of %s', V, triangle))
-    D = scene.incentre_point(equialteral, label='%s_2' % C.label, comment=LazyComment('Centre of %s', equialteral))
+    C1.opposite_side_constraint(C, line, comment=LazyComment('%s is outward of %s', C1, triangle))
+    D = scene.centre_point(equilateral, label='%s_2' % C.label)
 
 napoleonic(A, B, C)
 napoleonic(C, A, B)
