055580a08ce6cf0fc357ea2a482a866fb6e0332a
Nikolay Pultsin <geometer@fbreader.org>
2020-06-23 23:11:23 +0100

    useful properties in Reason class

--- python/sandbox/reason.py
+++ python/sandbox/reason.py
@@ -11,12 +11,33 @@ class Reason:
     def reset_premises(self):
         self.__all_premises = None
         self.__cost = None
+        self.__depth = None
+        self.__total_nodes = None
+        self.__unique_nodes = None
 
     @property
     def cost(self):
         if self.__cost is None:
             self.__cost = len(self.all_premises)
         return self.__cost
+
+    @property
+    def depth(self):
+        if self.__depth is None:
+            self.__depth = 1 + max((p.reason.depth for p in self.premises), default=0)
+        return self.__depth
+
+    @property
+    def total_nodes(self):
+        if self.__total_nodes is None:
+            self.__total_nodes = sum((p.reason.total_nodes for p in self.premises), 1)
+        return self.__total_nodes
+
+    @property
+    def unique_nodes(self):
+        if self.__unique_nodes is None:
+            self.__unique_nodes = len(self.all_premises)
+        return self.__unique_nodes
 
     @property
     def all_premises(self):
