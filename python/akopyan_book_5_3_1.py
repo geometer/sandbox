a61cb7199294d6d65f72d9dc59a9bf2693a4186f
Nikolay Pultsin <geometer@fbreader.org>
2020-06-28 01:59:36 +0100

    updated sample

--- python/akopyan_book_5_3_1.py
+++ python/akopyan_book_5_3_1.py
@@ -1,6 +1,6 @@
 from runner import run_sample
 from sandbox import Scene
-from sandbox.property import PointsCollinearityProperty
+from sandbox.property import PointsCollinearityProperty, PointOnLineProperty
 
 scene = Scene()
 
@@ -9,7 +9,9 @@ E = B.segment(C).free_point(label='E')
 B, E, F, K = scene.square(B, E, 'F', 'K')
 B.inside_constraint(A.segment(K))
 N = A.line_through(C).intersection_point(E.line_through(K), label='N')
+M = A.line_through(E).intersection_point(C.line_through(K), label='M')
 
-prop = PointsCollinearityProperty(D, N, F, True)
+#prop = PointsCollinearityProperty(D, N, F, True)
+prop = PointOnLineProperty(M, D.segment(F), True)
 
 run_sample(scene, prop)
