ecea3f4c0d60da73be1dd26c102b7c9512420cfe
Nikolay Pultsin <geometer@fbreader.org>
2020-07-03 23:16:01 +0100

    obesolete field => property-specific cache

--- python/sandbox/rules/cycle.py
+++ python/sandbox/rules/cycle.py
@@ -6,9 +6,16 @@ from .abstract import Rule, SingleSourceRule
 class CyclicOrderRule(SingleSourceRule):
     property_type = SameOrOppositeSideProperty
 
+    def __init__(self, context):
+        super().__init__(context)
+        self.processed = set()
+
+    def accepts(self, prop):
+        return prop not in self.processed
+
     def apply(self, prop):
-        if prop.reason.obsolete:
-            return
+        self.processed.add(prop)
+
         cycle0 = Cycle(*prop.segment.points, prop.points[0])
         cycle1 = Cycle(*prop.segment.points, prop.points[1])
         if not prop.same:
