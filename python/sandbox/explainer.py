0a2ba899b1dca70e4615a6d34b118c049fed27ce
Nikolay Pultsin <geometer@fbreader.org>
2020-07-03 06:48:16 +0100

    separate file for cycle-related rules

--- python/sandbox/explainer.py
+++ python/sandbox/explainer.py
@@ -11,6 +11,7 @@ from .rules.abstract import PredefinedPropertyRule, SyntheticPropertyRule
 from .rules.advanced import *
 from .rules.basic import *
 from .rules.circle import *
+from .rules.cycle import *
 from .rules.line import *
 from .rules.linear import *
 from .rules.quadrilateral import *
