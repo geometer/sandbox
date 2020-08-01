8553ca6ae253644774a825b1a07291c4eb1b1626
Nikolay Pultsin <geometer@fbreader.org>
2020-08-01 12:49:29 +0100

    compatibility

--- python/napoleon_inward.py
+++ python/napoleon_inward.py
@@ -14,9 +14,8 @@ def napoleonic(A, B, C):
     equilateral = scene.equilateral_triangle(A, B, '%s_1' % C.label)
     _, _, C1 = equilateral.points
     C1.comment = Comment('Third vertex of equilateral triangle with base $%{segment:AB}$', {'AB': A.segment(B)})
-    line = A.line_through(B, layer='auxiliary')
     comment = Comment('$%{triangle:equilateral}$ is facing into $%{triangle:triangle}$', {'equilateral': equilateral, 'triangle': triangle})
-    C1.same_side_constraint(C, line, comment=comment)
+    C1.same_side_constraint(C, A.segment(B), comment=comment)
     D = scene.centre_point(equilateral, label='%s_2' % C.label)
 
 napoleonic(A, B, C)
