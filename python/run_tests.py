8d4d21aa1943ffa26d50f2be8c011139f9a8cc10
Nikolay Pultsin <geometer@fbreader.org>
2021-04-27 04:58:38 +0100

    tests => sandbox_tests (to avoid conflicts with stanza)

--- python/run_tests.py
+++ python/run_tests.py
@@ -1,36 +1,36 @@
 import unittest
 
-from tests.misc.lengthratios import *
+from sandbox_tests.misc.lengthratios import *
 
-from tests.scene.midpoint import *
+from sandbox_tests.scene.midpoint import *
 
-from tests.explainer.altitudes import *
-from tests.explainer.inside_triangle import *
-from tests.explainer.perpendicular_bisector import *
-from tests.explainer.rog4578 import *
-from tests.explainer.rog4594 import *
-from tests.explainer.napoleon import *
-from tests.explainer.rightangled import *
-from tests.explainer.geo0324 import *
-from tests.explainer.geo0369 import *
-from tests.explainer.geo0374 import *
-from tests.explainer.lines import *
-from tests.explainer.misc import *
+from sandbox_tests.explainer.altitudes import *
+from sandbox_tests.explainer.inside_triangle import *
+from sandbox_tests.explainer.perpendicular_bisector import *
+from sandbox_tests.explainer.rog4578 import *
+from sandbox_tests.explainer.rog4594 import *
+from sandbox_tests.explainer.napoleon import *
+from sandbox_tests.explainer.rightangled import *
+from sandbox_tests.explainer.geo0324 import *
+from sandbox_tests.explainer.geo0369 import *
+from sandbox_tests.explainer.geo0374 import *
+from sandbox_tests.explainer.lines import *
+from sandbox_tests.explainer.misc import *
 
-from tests.meta.contradiction import *
+from sandbox_tests.meta.contradiction import *
 
-from tests.placement.sympy import *
-from tests.placement.intersection import *
-from tests.placement.napoleon import *
-from tests.placement.triangle345 import *
-from tests.placement.rog4578 import *
-from tests.placement.rog4580 import *
-from tests.placement.median import *
-from tests.placement.mccme7 import *
-from tests.placement.triangle_points import *
-from tests.placement.morley import *
-from tests.placement.pentagon import *
-from tests.placement.parallel import *
+from sandbox_tests.placement.sympy import *
+from sandbox_tests.placement.intersection import *
+from sandbox_tests.placement.napoleon import *
+from sandbox_tests.placement.triangle345 import *
+from sandbox_tests.placement.rog4578 import *
+from sandbox_tests.placement.rog4580 import *
+from sandbox_tests.placement.median import *
+from sandbox_tests.placement.mccme7 import *
+from sandbox_tests.placement.triangle_points import *
+from sandbox_tests.placement.morley import *
+from sandbox_tests.placement.pentagon import *
+from sandbox_tests.placement.parallel import *
 
 if __name__ == '__main__':
     unittest.main()
